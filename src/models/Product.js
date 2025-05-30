caa26dbe63b69b4495303d3a407dd78a1e8a959f
rsl23 <raoulkho05@gmail.com>
2025-05-30 09:45:56 +0700

    model buat product

---
+++ src/models/Product.js
@@ -0,0 +1,157 @@
+import { firestore } from "../config/database.js";
+import User from "./User.js"; 
+import { v4 as uuidv4 } from "uuid";
+
+class Product {
+  constructor({
+    product_id,
+    name,
+    price,
+    description,
+    image ="",
+    user = null,
+    created_at,
+    deleted_at = null,
+  }) {
+    this.product_id = product_id || uuidv4();
+    this.name = name;
+    this.price = price;
+    this.description = description;
+    this.image = image;
+    this.user = user; 
+    this.created_at = created_at || new Date().toISOString();
+    this.deleted_at = deleted_at;
+  }
+
+  static get productsRef() {
+    return firestore.collection("products");
+  }
+
+  static async create(product) {
+    if (!product.name || !product.price) {
+      throw new Error("Name, price are required.");
+    }
+    const newProduct = new Product(product);
+    try {
+      const snapshot = await Product.productsRef.get();
+      const count = snapshot.size + 1;
+      newProduct.product_id = `PD${String(count).padStart(3, "0")}`;
+    // const newRef = Product.productsRef.doc();
+    // newProduct.product_id = newRef.id;
+    } catch (error) {
+      console.error("Error generating new product ID:", error);
+      throw error;
+    }
+    try {
+      console.log("Creating product with data:", {
+        product_id: newProduct.product_id,
+        name: newProduct.name,
+        price: newProduct.price,
+        description: newProduct.description,
+        image: newProduct.image,
+        user: newProduct.user.id, 
+        created_at: newProduct.created_at,
+        deleted_at: newProduct.deleted_at,
+      });
+
+      await Product.productsRef.doc(newProduct.product_id).set({
+        name: newProduct.name,
+        price: newProduct.price,
+        description: newProduct.description,
+        image: newProduct.image,
+        user: newProduct.user.id, 
+        created_at: newProduct.created_at,
+        deleted_at: newProduct.deleted_at,
+      });
+
+      console.log(
+        "User successfully created in Firestore with ID:",
+        newProduct.product_id
+      );
+      return newProduct;
+    } catch (error) {
+      console.error("Error creating user in Firestore:", error);
+      throw error;
+    }
+  }
+
+  static async findProductById(product_id) {
+    try {
+      const doc = await Product.productsRef.doc(product_id).get();
+      if (doc.exists) {
+        const productData = doc.data();
+        if (productData.deleted_at) {
+          return null; 
+        }
+        return new Product({ id: doc.product_id, ...productData });
+      }
+      return null;
+    } catch (error) {
+      console.error("Error finding user by ID:", error);
+      throw error;
+    }
+  }
+
+  static async findProductByName(name) {
+  try {
+    const snapshot = await firestore.collection("products")
+      .where("deleted_at", "==", null)
+      .get();
+
+    const matchedProducts = [];
+
+    snapshot.forEach(doc => {
+      const data = doc.data();
+      if (data.name.toLowerCase().includes(name.toLowerCase())) {
+        matchedProducts.push(new Product({ product_id: doc.product_id, ...data }));
+      }
+    });
+
+    return matchedProducts;
+  } catch (error) {
+    console.error("Error finding products by name:", error);
+    throw error;
+  }
+}
+
+
+static async update(productId, updateData) {
+  const dataToUpdate = {
+    ...updateData,
+    updated_at: new Date().toISOString(),
+  };
+
+  delete dataToUpdate.product_id;
+  delete dataToUpdate.created_at;
+
+  try {
+    await Product.productsRef.doc(productId).update(dataToUpdate);
+    return await Product.findById(productId);
+  } catch (error) {
+    console.error("Error updating product:", error);
+    throw error;
+  }
+}
+
+
+  static async softDelete(productId) {
+  try {
+    await Product.productsRef.doc(productId).update({
+      deleted_at: new Date().toISOString(),
+      updated_at: new Date().toISOString(),
+    });
+    return true;
+  } catch (error) {
+    console.error("Error soft deleting product:", error);
+    throw error;
+  }
+}
+
+
+  toJSON() {
+    const { ...productData } = this;
+    return productData;
+  }
+}
+
+export default Product;
